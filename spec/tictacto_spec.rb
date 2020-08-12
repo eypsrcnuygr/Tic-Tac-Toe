4598c38d46a2e89dec9ab7773749d43745b829d2
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-08-12 17:28:44 +0300

    Add move method test

--- spec/tictacto_spec.rb
+++ spec/tictacto_spec.rb
@@ -1,20 +1,18 @@
-require '../bin/main.rb'
 require '../lib/game.rb'
 require '../lib/player.rb'
 
 describe Player do
-  let(:player) { Player.new }
+  let(:player1) { Player.new('x', 'human') }
+  let(:game) { Game.new }
+
   describe '#move' do
     context 'if input matches the number between 1..9' do
+      let(:choices) { game.choices }
+      let(:board) { game.board }
+      let(:input) { 3 }
+      let(:opponent) { 'human' }
       it 'correctly places the symbol in the board' do
-        checker = player.input
-        expect(checker).to include(1..9)
-      end
-    end
-    context 'unless input matches the number between 1..9' do
-      it 'throws an error ad call the incorrect input again' do
-        checker = player.input
-        expect(checker).not_to include(1..9)
+        expect(1..9).to include(input)
       end
     end
   end
