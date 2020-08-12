da86bf0bb0c8d96ce7c85c87271ca852005337ac
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-08-12 18:42:56 +0300

    Add condition checker

--- spec/tictacto_spec.rb
+++ spec/tictacto_spec.rb
@@ -16,4 +16,27 @@ describe Player do
       end
     end
   end
+
+  describe '#choose_opponent' do
+    let(:opponent) { player1.opponent_choice }
+    it 'throws error if the opponent is not human or cpu' do
+      expect(opponent).to eql 'human' || 'cpu'
+    end
+  end
+end
+
+describe Game do
+  let(:game) { Game.new }
+  let(:board) { %w[x x o x o _ x _ _] }
+  let(:symbol) { 'x' }
+
+  describe '#condition_checker' do
+    it 'expects a linear winning case' do
+      expect(board[0..2]).to eql board.each_slice(3).to_a[0] ||
+                                 board.each_slice(3).to_a.transpose[0]
+    end
+    it 'expects a diagonal winning case' do
+      expect(true).to eql game.diagonal(%w[x x o o x _ o _ x], 'x')
+    end
+  end
 end
