b5ad27dba6e2a8430171b666ab07b278358f9518
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-08-12 16:22:44 +0300

    Not working test

--- spec/tictacto_spec.rb
+++ spec/tictacto_spec.rb
@@ -1,15 +1,21 @@
-require './bin/main.rb'
+require '../bin/main.rb'
 require '../lib/game.rb'
 require '../lib/player.rb'
 
 describe Player do
-  let (:player) { Player.new }
+  let(:player) { Player.new }
   describe '#move' do
     context 'if input matches the number between 1..9' do
       it 'correctly places the symbol in the board' do
-        checker = player.input.match?(/\b[1-9]\b/)
-        expect(checker).to include 1..9
+        checker = player.input
+        expect(checker).to include(1..9)
       end
-    end 
+    end
+    context 'unless input matches the number between 1..9' do
+      it 'throws an error ad call the incorrect input again' do
+        checker = player.input
+        expect(checker).not_to include(1..9)
+      end
+    end
   end
-end 
+end
